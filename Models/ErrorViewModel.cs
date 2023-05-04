cefa7b15c1aef8574848d5921320ee7456e78f31
anhlamdz <tuivjpvl@gmail.com>
2023-05-04 10:44:22 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace quanlydoanvien.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
